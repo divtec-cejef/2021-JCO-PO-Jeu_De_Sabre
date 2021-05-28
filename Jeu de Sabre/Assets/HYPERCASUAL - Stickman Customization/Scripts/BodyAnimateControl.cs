6db8c173eb6cd6f0815aed0b0448b06c410da244
Léo Küttel <leo.kuettel@divtec.ch>
2021-05-28 16:15:50 +0200

    Changement des personnages

---
+++ Jeu de Sabre/Assets/HYPERCASUAL - Stickman Customization/Scripts/BodyAnimateControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class BodyAnimateControl : MonoBehaviour
+{
+    public Animator modelAnimator;
+    public Text buttonText;
+
+    private bool _isAnimate = false;
+
+
+    public void AnimateButtonClick()
+    {
+        if (!_isAnimate)
+        {
+            modelAnimator.Play("Walk");
+            buttonText.text = "STOP";
+        }
+        else
+        {
+            modelAnimator.Play("TPose");
+            buttonText.text = "WALK";
+        }
+        _isAnimate = !_isAnimate;
+    }
+}
