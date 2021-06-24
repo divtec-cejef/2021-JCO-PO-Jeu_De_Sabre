5938540d45d1322eae0f393ac5cd634ee77a890f
Doryan Etique <doryan.etique@divtec.ch>
2021-06-24 21:30:30 +0200

    Ajout : Ecran de chargement

--- Jeu de Sabre/Assets/MenuOption.cs
+++ Jeu de Sabre/Assets/MenuOption.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuOption : MonoBehaviour
 {
+
+    public GameObject menuUi;
+    public GameObject loadingUi;
+    public GameObject playerStuff;
+    public Slider loadingBar;
+    public TextMeshProUGUI progressText;
+    
     public void OnQuit()
     {
         Application.Quit();
@@ -12,7 +21,28 @@ public class MenuOption : MonoBehaviour
 
     public void OnLaunch()
     {
-        SceneManager.LoadScene("Map");
+        StartCoroutine(loadLevel());
     }
+
+
+    private IEnumerator loadLevel()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync("Map");
+
+        menuUi.SetActive(false);
+        playerStuff.SetActive(false);
+        loadingUi.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+
+            loadingBar.value = progress;
+            progressText.text = (int)(progress * 100f) + "%";
+            
+            yield return null;
+        }
+    }
+    
     
 }
